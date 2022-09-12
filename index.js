67aa25bea5c5486e300bdf9bfc991a7be6abbbee
lclcada <vittalinni@gmail.com>
2022-09-12 19:23:48 -0300

    descricao

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-//
+var ctx = null;
+var tileW = 40, tileH = 40;
+var mapW = 10, mapH = 10;
+
+var currentSecond = 0, frameCount = 0, framesLastSecond = 0;
+
+var gameMap = [
+    0, 0, 0, 0, 0, 0, 0
+]
+
+window.onload = function()
+{
+    ctx.document.getElementById("jogo").getContext("2d");
+    requestAnimationFrame(drawGame);
+    ctx.font = "bold 10pt sans-serif";
+}
+
+function drawGame()
+{
+    if(ctx == null) {return;}
+    
+}
